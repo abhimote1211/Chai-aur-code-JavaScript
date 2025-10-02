cdf1662a75fde5349eb40b3ae49fdf6494e27e32
Abhishek Mote <abhishekmote1211@gmail.com>
2025-10-02 18:14:01 +0000

    datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "abhishekmote1211@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"  
+
+//accountId = 2 not allowed
+
+accountEmail = "abhi1211@gmail.com"
+accountPassword = "67890"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// alert(3+3)   //we are using nodejs, not browser
+
+console.log( 3 
+    +
+     3 );  //code readability should be high
+console.log("Abhishek"); 
+
+//Data Types in JS
+
+let name = "Abhishek Mote";  //string
+let age = 21;  //number
+let isApproved = true;  //boolean
+let firstName = undefined;  //undefined
+let selectedColor = null;  //null
+
+//Premitive data types:
+// number => 2 to power 53
+// string => "Abhishek"
+// boolean => true/false
+// undefined => variable not assigned a value
+// null => variable is empty
+// symbol => unique identifier 
+// bigint => larger than 2 to power 53
+
+//Non-premitive data types:
+// object => key-value pairs
+// array => collection of items
+
+console.log(typeof name);
+console.log(typeof null);  //bug in JS, it should return 'object'
+console.log(typeof undefined);
+console.log(typeof age);
+console.log(typeof isApproved);
