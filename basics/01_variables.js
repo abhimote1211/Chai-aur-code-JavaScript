f6a08cb066302d4ee2d920673a3f3aa54242119e
Abhishek Mote <abhishekmote1211@gmail.com>
2025-10-05 06:52:27 +0000

    Data Types

--- basics/01_variables.js
+++ basics/01_variables.js
@@ -14,3 +14,20 @@ accountCity = "Mumbai"
 console.log(accountId);
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState, accountCountry]);
+
+//variable names are case sensitive
+//cannot start with number
+//cannot have space or hyphen (-)
+//cannot be a reserved keyword like var, let, const, if, else, return, function
+//use camelCase
+//use meaningful names
+//use _ if needed
+//use uppercase for constants that never change like PI = 3.14, API_KEY = "1234567890"
+//use lowercase for other variables like accountId, accountEmail, accountPassword
+//use uppercase for global variables like BASE_URL = "https://api.example.com"
+// var is function scoped
+// let and const are block scoped
+// avoid using var
+// use const by default, use let if you need to reassign
+// avoid using global variables
+// use strict mode "use strict"; at the top of the file to avoid accidental global variables
